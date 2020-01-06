75eb7b85ccbccc85efb4a8da66a5ddcd287d83df
Bruno Balduccio <bbalduccio@genuinegenius.com>
2020-01-06 09:42:13 -0300

    Added logic for Get with Parameters (dateFrom,dateTo,accountFrom) Deleted all trace of weatherForecastExample Changed /transactions to the default URL when excecuting the solution

---
+++ BankingSystem/Controllers/TransactionsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+//using System.Web.Http;
+using BankingSystem.ViewModels;
+using BSDatabaseContext;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BankingSystem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransactionsController : ControllerBase
+    {
+        [HttpGet]
+        public List<TransactionVM> Get(DateTime dateFrom, DateTime dateTo,int accountFrom)
+        {
+            if (dateTo == DateTime.MinValue)
+                dateTo = DateTime.MaxValue;
+            TransactionDataAccess tda = new TransactionDataAccess();
+            var transactions = tda.GetAllList().Where(t=>t.DateAndTime> dateFrom && t.DateAndTime< dateTo && (t.AccountFrom.AccountId==accountFrom||accountFrom==0)).Select(t => new TransactionVM()
+            {
+                TransactionId = t.TransactionId,
+                AccountFrom = t.AccountFrom.AccountId,
+                AccountTo = t.AccountTo.AccountId,
+                DateAndTime = t.DateAndTime,
+                Description = t.Description,
+                Amount = t.Amount
+            }).ToList();  
+            return transactions;
+            }
+        }
+    }
+
